cbe0484e52321e4a4b5bfe23fbf68b994329c339
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-07-10 00:56:34 +0530

    Time: 606 ms (10.45%) | Memory: 359.3 MB (9.48%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,25 +1,4 @@
 class Solution {
-    bool dfs(unordered_map<int,vector<int>>&graph,int source, int destination,vector<bool>&vis,int n){
-
-        if(source == destination){
-            return true;
-        }
-
-        vis[source] = true;
-
-        for(auto neighbor:graph[source]){
-            if(!vis[neighbor]){
-               if( dfs(graph,neighbor,destination,vis,n)){
-                return true;
-               }
-            }
-        }
-        return false;
-
-    }
-
-
-
 public:
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
 
@@ -28,13 +7,41 @@ public:
         for(auto x:edges){
             int u = x[0];
             int v = x[1];
+
             graph[u].push_back(v);
             graph[v].push_back(u);
-
         }
 
         vector<bool>vis(n,false);
-        return dfs(graph,source,destination,vis,n);
+
+        vis[source] = true;
+
+        queue<int>q;
+        q.push(source);
+
+        while(!q.empty()){
+            int curr = q.front();
+
+            q.pop();
+
+            if(curr == destination) return true;
+
+            for(int neighbor:graph[curr]){
+                if(!vis[neighbor]){
+                    q.push(neighbor);
+
+                    vis[neighbor] = true;
+                }
+            }
+
+
+
+
+
+        }
+
+
+        return false;
         
     }
 };
