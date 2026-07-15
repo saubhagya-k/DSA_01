cd882c8c6da35d6abbe799cddab5d4c1e496b3f8
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-07-15 18:49:03 +0530

    Time: 3 ms (16.66%) | Memory: 8.5 MB (85.02%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -1,12 +1,19 @@
-#include <numeric> // Required for std::gcd
-
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) {
-        long long sumOdd = 0;
-        long long sumEven = 0;
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
 
-        // The first n odd and even numbers fall within the range 1 to 2*n
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        
+        // Loop to process the first n odd and n even elements (total 2*n numbers)
         for (int i = 1; i <= 2 * n; i++) {
             if (i % 2 == 0) {
                 sumEven += i;
@@ -14,8 +21,8 @@ public:
                 sumOdd += i;
             }
         }
-
-        // Compute GCD using modern standard library features
-        return std::gcd(sumOdd, sumEven);
+        
+        // Added the missing return keyword
+        return gcd(sumOdd, sumEven); 
     }
 };
