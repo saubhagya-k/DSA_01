4066b12edf22bd435671a3d92b71c3bf0d0e6fb0
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-07-18 17:31:54 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (35.13%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int n = s.length();
+
+        int count = 0;
+
+
+        reverse(s.begin(),s.end());
+
+        for(int i=0;i<n;i++){
+           
+
+            if (s[i] != ' ') {
+                count++; 
+            } 
+            else {
+                
+                if (count > 0) {
+                    break;
+                }
+            }
+
+        }
+
+        return count;
+        
+
+    }
+};
