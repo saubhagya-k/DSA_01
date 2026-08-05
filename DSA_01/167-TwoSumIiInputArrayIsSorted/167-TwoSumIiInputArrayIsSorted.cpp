7dd62d1395b70c49ab4fb1ec685d451399bd4b9c
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:59:57 +0530

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ DSA_01/167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.cpp
@@ -0,0 +1,26 @@
+// Last updated: 05/08/2026, 16:59:57
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+
+        unordered_map<int,int>map;
+
+        
+
+        int n = numbers.size();
+
+        for(int i=0;i<n;i++){
+            if(map.find(target-numbers[i])!=map.end()){
+
+              
+
+                return {map[target-numbers[i]],i+1};
+                
+            }
+            map[numbers[i]] = i+1;
+        }
+
+        return {};
+        
+    }
+};
