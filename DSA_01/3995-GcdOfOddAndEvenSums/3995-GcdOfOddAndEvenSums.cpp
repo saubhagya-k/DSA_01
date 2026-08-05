f1e21190d7628c59926bbab3b06825b75dc34e69
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:51:40 +0530

    Create 3995-GcdOfOddAndEvenSums

---
+++ DSA_01/3995-GcdOfOddAndEvenSums/3995-GcdOfOddAndEvenSums.cpp
@@ -0,0 +1,29 @@
+// Last updated: 05/08/2026, 16:51:40
+class Solution {
+public:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    int gcdOfOddEvenSums(int n) {
+        int sumOdd = 0;
+        int sumEven = 0;
+        
+        // Loop to process the first n odd and n even elements (total 2*n numbers)
+        for (int i = 1; i <= 2 * n; i++) {
+            if (i % 2 == 0) {
+                sumEven += i;
+            } else {
+                sumOdd += i;
+            }
+        }
+        
+        // Added the missing return keyword
+        return gcd(sumOdd, sumEven); 
+    }
+};
