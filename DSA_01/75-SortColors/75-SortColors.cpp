451ce4e80e39d73f6bc63751046ed95bb614a8b6
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:02:06 +0530

    Create 75-SortColors

---
+++ DSA_01/75-SortColors/75-SortColors.cpp
@@ -0,0 +1,13 @@
+// Last updated: 05/08/2026, 17:02:06
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        
+        
+
+        sort(nums.begin(),nums.end());
+
+       
+    }
+};
