d2e2dbd637e516dd587e98d7f880a4b877a71df7
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:54:56 +0530

    Create 978-ValidMountainArray

---
+++ DSA_01/978-ValidMountainArray/978-ValidMountainArray.cpp
@@ -0,0 +1,24 @@
+// Last updated: 05/08/2026, 16:54:56
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int i = 0;
+        int n = arr.size();
+
+       
+        while(i < n-1 && arr[i] < arr[i+1]){
+            i++;
+        }
+
+        
+        if(i == 0 || i == n-1) return false;
+
+       
+        while(i < n-1 && arr[i] > arr[i+1]){
+            i++;
+        }
+
+        
+        return i==n-1;
+    }
+};
