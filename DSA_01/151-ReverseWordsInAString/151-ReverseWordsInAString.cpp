0d22947f7b2e1e2385d198373f0bb02a358be780
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:00:11 +0530

    Create 151-ReverseWordsInAString

---
+++ DSA_01/151-ReverseWordsInAString/151-ReverseWordsInAString.cpp
@@ -0,0 +1,33 @@
+// Last updated: 05/08/2026, 17:00:11
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
+
+        reverse(s.begin(), s.end());
+
+        for (int i = 0; i < n; i++) {
+            string word = "";
+
+            while (i < n && s[i] != ' ') {
+                word += s[i];
+                i++;
+            }
+
+            reverse(word.begin(), word.end());
+            
+            if (word.length() > 0) {
+               
+                if (ans.length() == 0) {
+                    ans += word;
+                } else {
+                    ans += " " + word;
+                }
+            }
+        }
+        
+        
+        return ans; 
+    }
+};
