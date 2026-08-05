832d26ea6b6ef070be45b9239cd172adfc016851
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:58:27 +0530

    Create 242-ValidAnagram

---
+++ DSA_01/242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,19 @@
+// Last updated: 05/08/2026, 16:58:27
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int n1  = s.length();
+        int n2  = t.length();
+
+        if(n1 != n2){
+            return false;
+        }
+
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+
+        return s==t;
+        
+    }
+};
