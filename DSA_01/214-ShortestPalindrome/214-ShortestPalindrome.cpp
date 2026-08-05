5c749f0104ba4b37a4e08bd5c58fa87cd420500e
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:58:57 +0530

    Create 214-ShortestPalindrome

---
+++ DSA_01/214-ShortestPalindrome/214-ShortestPalindrome.cpp
@@ -0,0 +1,25 @@
+// Last updated: 05/08/2026, 16:58:56
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+
+        string rev = s;
+
+        reverse(rev.begin(),rev.end());
+
+        for(int i=0;i<s.length();i++){
+            if(!memcmp(s.c_str(),rev.c_str()+i,s.length()-i)){
+                return rev.substr(0,i)+s;
+            }
+        }
+
+        return rev+s;
+
+
+
+
+
+        
+        
+    }
+};
