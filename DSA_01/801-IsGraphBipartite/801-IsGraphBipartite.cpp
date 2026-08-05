2ceee79b9c51c69b59c30356c8e80632cf82df37
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:55:36 +0530

    Create 801-IsGraphBipartite

---
+++ DSA_01/801-IsGraphBipartite/801-IsGraphBipartite.cpp
@@ -0,0 +1,48 @@
+// Last updated: 05/08/2026, 16:55:35
+class Solution {
+public:
+
+bool helperColored(vector<vector<int>>& graph,int curr,vector<int>&colored,int currColor){
+
+    colored[curr] = currColor;
+
+    for(int &br : graph[curr]){
+        if(colored[br] == currColor){
+            return false;
+        }
+
+        if(colored[br] == -1){
+            int currcurrColor = 1 - currColor;
+
+            if(helperColored(graph,br,colored,currcurrColor)==false){
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+    bool isBipartite(vector<vector<int>>& graph) {
+
+        int n = graph.size();
+
+        vector<int>colored(n,-1);
+
+
+        for(int i=0;i<n;i++){
+
+            if(colored[i]==-1){
+            if(helperColored(graph,i,colored,1)==false){
+
+                return false;
+
+            }
+        }
+        }
+
+        return true;
+
+        
+        
+    }
+};
