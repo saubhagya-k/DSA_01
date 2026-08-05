c71c53f494f5093a79fa2ef8cd804dc81aec7d26
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:56:51 +0530

    Create 547-NumberOfProvinces

---
+++ DSA_01/547-NumberOfProvinces/547-NumberOfProvinces.cpp
@@ -0,0 +1,50 @@
+// Last updated: 05/08/2026, 16:56:50
+class Solution {
+public:
+
+void helper(unordered_map<int,vector<int>>&adj, vector<bool>&visited,int u){
+
+    visited[u] = true;
+
+    for(int &v:adj[u]){
+        if(!visited[v]){
+            helper(adj,visited,v);
+        }
+
+    }
+
+}
+    int findCircleNum(vector<vector<int>>& isConnected) {
+
+    int n = isConnected.size();
+
+        int count = 0;
+
+        vector<bool>visited(n,false);
+
+        unordered_map<int,vector<int>>adj;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                helper(adj,visited,i);
+
+                count++;
+
+            }
+        }
+
+        return count;
+        
+    }
+};
