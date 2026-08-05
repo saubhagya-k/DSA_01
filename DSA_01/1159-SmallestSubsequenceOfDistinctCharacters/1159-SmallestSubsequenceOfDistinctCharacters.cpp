8b1d24e53e861b1afb2690a1e68a8792f909bcd7
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:54:11 +0530

    Create 1159-SmallestSubsequenceOfDistinctCharacters

---
+++ DSA_01/1159-SmallestSubsequenceOfDistinctCharacters/1159-SmallestSubsequenceOfDistinctCharacters.cpp
@@ -0,0 +1,50 @@
+// Last updated: 05/08/2026, 16:54:11
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+
+         vector<bool>after(26,false);
+
+        vector<int>taken(26);
+
+        string result = "";
+
+       int n = s.length();
+
+       for(int i=0;i<n;i++){
+        char c = s[i];
+
+        taken[c-'a'] = i;
+       }
+
+       for(int i = 0;i<n;i++){
+
+        char ch = s[i];
+        int idx = ch - 'a';
+
+
+        if(after[idx] == true){
+            continue;
+        }
+
+        while(result.length()>0 && result.back()>ch && taken[result.back()-'a']>i){
+            after[result.back()-'a'] = false;
+
+            result.pop_back();
+        }
+
+        result.push_back(ch);
+
+        after[idx] = true;
+
+
+       }
+
+       return result;
+
+        
+
+
+        
+    }
+};
