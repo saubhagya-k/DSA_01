1b3247750e9a83683a746df9da5a64fd1f7b1a1b
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:01:45 +0530

    Create 96-UniqueBinarySearchTrees

---
+++ DSA_01/96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.cpp
@@ -0,0 +1,18 @@
+// Last updated: 05/08/2026, 17:01:44
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1, 0);
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int nodes = 2; nodes <= n; nodes++){
+            for(int root = 1; root <= nodes; root++){
+                dp[nodes] += dp[root-1] * dp[nodes-root];
+            }
+        }
+
+        return dp[n];
+    }
+};
