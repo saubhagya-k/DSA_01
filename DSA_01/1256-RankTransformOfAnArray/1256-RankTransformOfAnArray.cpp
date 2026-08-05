f3671e28280130fa82fe4a924ea8e947b4a58f19
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:53:53 +0530

    Create 1256-RankTransformOfAnArray

---
+++ DSA_01/1256-RankTransformOfAnArray/1256-RankTransformOfAnArray.cpp
@@ -0,0 +1,29 @@
+// Last updated: 05/08/2026, 16:53:52
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+
+        set<int>a(arr.begin(),arr.end());
+
+        unordered_map<int,int>rank;
+
+        int ranks = 1;
+
+
+        for(int x:a){
+
+            rank[x] = ranks++;
+
+        }
+
+
+        vector<int>ans;
+
+        for(int y:arr){
+            ans.push_back(rank[y]);
+        }
+
+        return ans;
+        
+    }
+};
