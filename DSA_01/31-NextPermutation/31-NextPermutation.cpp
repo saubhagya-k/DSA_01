24584335ef6ce055e9b258dd56bba8aab4a52977
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:02:49 +0530

    Create 31-NextPermutation

---
+++ DSA_01/31-NextPermutation/31-NextPermutation.cpp
@@ -0,0 +1,13 @@
+// Last updated: 05/08/2026, 17:02:48
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
