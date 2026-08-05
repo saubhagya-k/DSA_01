ead77882074847f95d8e24e2091ac67d5ffdd853
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:57:45 +0530

    Create 383-RansomNote

---
+++ DSA_01/383-RansomNote/383-RansomNote.cpp
@@ -0,0 +1,32 @@
+// Last updated: 05/08/2026, 16:57:45
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        if(ransomNote.length()>magazine.length()){
+            return false;
+        }
+
+        vector<int>ans(26,0);
+
+        for(char X: magazine){
+            ans[X - 'a']++;
+
+        }
+
+        for(char X: ransomNote){
+            ans[X - 'a']--;
+
+            if(ans[X - 'a'] <0){
+                return false;
+            }
+        }
+
+        return true;
+
+
+
+      
+        
+    }
+};
