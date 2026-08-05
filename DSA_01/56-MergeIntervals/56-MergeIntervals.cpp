b982eec9240e9969d47fad58307b83f068b4b9d6
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:02:32 +0530

    Create 56-MergeIntervals

---
+++ DSA_01/56-MergeIntervals/56-MergeIntervals.cpp
@@ -0,0 +1,30 @@
+// Last updated: 05/08/2026, 17:02:32
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>>ans;
+
+        ans.push_back(intervals[0]);
+
+        int n = intervals.size();
+
+        for(int i=1;i<n;i++){
+
+            if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            }
+
+            else{
+                ans.push_back(intervals[i]);
+            }
+
+        }
+        return ans;
+
+
+        
+    }
+};
