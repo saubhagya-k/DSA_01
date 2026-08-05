cba53f3c2c5a808c6e2894a3afa3516600ec077f
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:56:04 +0530

    Create 724-FindPivotIndex

---
+++ DSA_01/724-FindPivotIndex/724-FindPivotIndex.cpp
@@ -0,0 +1,29 @@
+// Last updated: 05/08/2026, 16:56:04
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int totalSum = 0;
+
+        int n = nums.size();
+
+        for(int i=0;i<n;i++){
+            totalSum += nums[i];
+        }
+
+        for(int i=0;i<n;i++){
+            rightSum = totalSum-leftSum-nums[i];
+
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+
+        }
+        return -1;
+        
+    }
+};
