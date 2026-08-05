5c929421f9cd625cb752463544c733af2cea1608
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:52:51 +0530

    Create 2121-FindIfPathExistsInGraph

---
+++ DSA_01/2121-FindIfPathExistsInGraph/2121-FindIfPathExistsInGraph.cpp
@@ -0,0 +1,48 @@
+// Last updated: 05/08/2026, 16:52:51
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+
+        unordered_map<int,vector<int>>graph;
+
+        for(auto x:edges){
+            int u = x[0];
+            int v = x[1];
+
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        vector<bool>vis(n,false);
+
+        vis[source] = true;
+
+        queue<int>q;
+        q.push(source);
+
+        while(!q.empty()){
+            int curr = q.front();
+
+            q.pop();
+
+            if(curr == destination) return true;
+
+            for(int neighbor:graph[curr]){
+                if(!vis[neighbor]){
+                    q.push(neighbor);
+
+                    vis[neighbor] = true;
+                }
+            }
+
+
+
+
+
+        }
+
+
+        return false;
+        
+    }
+};
