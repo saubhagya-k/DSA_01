8e40e75e6658efc78967f0de02df24dd90830cb0
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 16:55:30 +0530

    Create 817-DesignHashmap

---
+++ DSA_01/817-DesignHashmap/817-DesignHashmap.cpp
@@ -0,0 +1,30 @@
+// Last updated: 05/08/2026, 16:55:29
+class MyHashMap {
+public:
+
+    vector<int> arr;
+
+    MyHashMap() {
+        arr.resize(1000001, -1);
+    }
+
+    void put(int key, int value) {
+        arr[key] = value;
+    }
+
+    int get(int key) {
+        return arr[key];
+    }
+
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
