7458436d7a13fbce18afc38fe8f211dea228e7f3
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-05 17:01:02 +0530

    Create 114-FlattenBinaryTreeToLinkedList

---
+++ DSA_01/114-FlattenBinaryTreeToLinkedList/114-FlattenBinaryTreeToLinkedList.cpp
@@ -0,0 +1,33 @@
+// Last updated: 05/08/2026, 17:01:01
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+    TreeNode* curr = root;
+
+    while (curr != NULL) {
+        if (curr->left != NULL) {
+            TreeNode* temp = curr->left;
+
+            while (temp->right != NULL) {
+                temp = temp->right;
+            }
+
+            temp->right = curr->right;
+            curr->right = curr->left;
+            curr->left = NULL;
+        }
+        curr = curr->right;
+    }
+}
+};
