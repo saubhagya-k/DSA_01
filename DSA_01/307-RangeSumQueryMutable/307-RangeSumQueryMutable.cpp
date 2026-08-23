7dab1168609180085ec77e2e1d797415a491d866
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-23 18:28:43 +0530

    Create 307-RangeSumQueryMutable

---
+++ DSA_01/307-RangeSumQueryMutable/307-RangeSumQueryMutable.cpp
@@ -0,0 +1,90 @@
+// Last updated: 23/08/2026, 18:28:43
+class NumArray {
+public:
+
+int n;
+vector<int>segmentTree;
+
+void buildSegmentTree(int i,int l,int r,vector<int>& nums){
+
+    if(l==r){
+        segmentTree[i] = nums[l];
+        return;
+    }
+
+    int mid = l+ (r-l)/2;
+
+    buildSegmentTree(2*i+1,l,mid,nums);
+    buildSegmentTree(2*i+2,mid+1,r,nums);
+
+    segmentTree[i] = segmentTree[2*i+1]+segmentTree[2*i+2];
+
+}
+
+void updateSgementTree(int index,int val,int i,int l,int r){
+
+    if(l==r){
+        segmentTree[i] = val;
+
+        return;
+    }
+
+    int mid =  l+(r-l)/2;
+
+    if(index<=mid){
+        updateSgementTree(index,val,2*i+1,l,mid);
+    }
+    else{
+        updateSgementTree(index,val,2*i+2,mid+1,r);
+    }
+
+    segmentTree[i] = segmentTree[2*i+1]+segmentTree[2*i+2];
+
+}
+
+int sumRangeSegmentTree(int left,int right,int i,int l,int r){
+
+
+    if(l>right || r<left){
+        return 0;
+    }
+
+    if(l>=left && r<=right){
+        return segmentTree[i];
+    }
+
+    int mid = l+(r-l)/2;
+
+    return sumRangeSegmentTree(left,right,2*i+1,l,mid)+sumRangeSegmentTree(left,right,2*i+2,mid+1,r);
+
+
+
+
+}
+    NumArray(vector<int>& nums) {
+
+        n = nums.size();
+
+        segmentTree.resize(4*n);
+        buildSegmentTree(0,0,n-1,nums);
+        
+    }
+    
+    void update(int index, int val) {
+        
+        updateSgementTree(index,val,0,0,n-1);
+    }
+    
+    int sumRange(int left, int right) {
+
+       return  sumRangeSegmentTree(left,right,0,0,n-1);
+        
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */
