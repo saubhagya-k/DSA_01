756cc2c36cf80c69cfc7722da7352aeb31bf7af3
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-08-23 18:24:48 +0530

    Create 4033-LongestSubsequenceWithNonZeroBitwiseXor

---
+++ DSA_01/4033-LongestSubsequenceWithNonZeroBitwiseXor/4033-LongestSubsequenceWithNonZeroBitwiseXor.cpp
@@ -0,0 +1,22 @@
+// Last updated: 23/08/2026, 18:24:48
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int n = nums.size();
+        int totalXor = 0;
+        bool allZero = true;
+
+        for (int x : nums) {
+            totalXor ^= x;
+            if (x > 0) {
+                allZero = false;
+            }
+        }
+
+        if (totalXor > 0) {
+            return n;
+        }
+
+        return allZero ? 0 : n - 1;
+    }
+};
