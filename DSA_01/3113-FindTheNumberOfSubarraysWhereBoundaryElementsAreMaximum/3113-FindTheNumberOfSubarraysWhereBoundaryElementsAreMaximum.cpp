26e31947850ad7c688dea6255a92bb785b0cde2d
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-09-08 11:32:33 +0530

    Create 3113-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum

---
+++ DSA_01/3113-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum/3113-FindTheNumberOfSubarraysWhereBoundaryElementsAreMaximum.cpp
@@ -0,0 +1,32 @@
+// Last updated: 08/09/2026, 11:32:33
+class Solution {
+public:
+    long long numberOfSubarrays(vector<int>& nums) {
+
+        stack<int>st;
+
+        long long total = 0;
+
+        unordered_map<int,int>mp;
+
+        for(int x:nums){
+            while(!st.empty() && st.top()<x){
+                mp.erase(st.top());
+                st.pop();
+               
+            }
+
+            st.push(x);
+            mp[x]++;
+
+
+            total += mp[x];
+
+        }
+
+        return total;
+
+        
+        
+    }
+};
