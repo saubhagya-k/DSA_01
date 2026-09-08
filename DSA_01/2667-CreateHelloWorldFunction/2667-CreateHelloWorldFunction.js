2407487e8b4343b1f999a5af3c82a2a20ceca8a8
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-09-08 11:33:20 +0530

    Create 2667-CreateHelloWorldFunction

---
+++ DSA_01/2667-CreateHelloWorldFunction/2667-CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+// Last updated: 08/09/2026, 11:33:20
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
