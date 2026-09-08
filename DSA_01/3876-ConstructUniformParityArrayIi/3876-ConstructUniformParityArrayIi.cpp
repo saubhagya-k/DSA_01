e1123bc15c69e0c003280f38f2e9ce3c0624d8e8
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-09-08 11:31:29 +0530

    Create 3876-ConstructUniformParityArrayIi

---
+++ DSA_01/3876-ConstructUniformParityArrayIi/3876-ConstructUniformParityArrayIi.cpp
@@ -0,0 +1,20 @@
+// Last updated: 08/09/2026, 11:31:29
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn = nums1[0];
+        bool hasOdd = false;
+        for (int v : nums1) {
+            if (v < mn) {
+                mn = v;
+            }
+            if (v & 1) {
+                hasOdd = true;
+            }
+        }
+        if (mn & 1) {
+            return true;
+        }
+        return !hasOdd;
+    }
+};
