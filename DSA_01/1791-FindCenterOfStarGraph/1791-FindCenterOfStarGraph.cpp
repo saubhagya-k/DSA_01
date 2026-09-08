e1e56512c17de96637f3c7a08009d7465863a9fb
saubhagya-keshav-singh <saubhagya6011@gmail.com>
2026-09-08 11:34:34 +0530

    Create 1791-FindCenterOfStarGraph

---
+++ DSA_01/1791-FindCenterOfStarGraph/1791-FindCenterOfStarGraph.cpp
@@ -0,0 +1,18 @@
+// Last updated: 08/09/2026, 11:34:34
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        
+        int a = edges[0][0];
+        int b = edges[0][1];
+        int c = edges[1][0];
+        int d = edges[1][1];
+
+      
+        if (a == c || a == d) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+};
